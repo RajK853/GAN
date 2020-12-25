8925ef6ef0e57674d2e01feb58fa1611913fc362
Raj K <rgk.rana@gmail.com>
2020-12-25 12:18:00 +0100

    Major changes
    - Added Keras callback to visualize model graphs in tensorboard
    - Implemented new batch data fetching method
    - Renamed gan_model into combined_model

--- src/models/base.py
+++ src/models/base.py
@@ -2,43 +2,76 @@ import os
 import numpy as np
 from datetime import datetime
 from collections import defaultdict
-
 from rich.console import Console
-from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
+from tensorflow.compat.v1.keras import callbacks
+
 
 
 class BaseGAN:
-    def __init__(self, dataset, img_shape, latent_size, log_prefix="log_", num_evaluates=10, console=None):
+    def __init__(self, dataset, class_labels, img_shape, latent_size, log_prefix="log_", num_evaluates=10, console=None):
         self.dataset = dataset
+        self.class_labels = class_labels
         self.dataset_size = dataset.shape[0]
         self.img_shape = img_shape
         self.latent_size = latent_size
         self.num_evaluates = num_evaluates
         self.log_prefix = log_prefix
         self.history = defaultdict(list)
-        
+        # Init log directory
         current_time = datetime.now().strftime("%d.%m.%Y_%H.%M")
         self.log_dir = os.path.join("logs", f"{self.__class__.__name__}_{current_time}")
         self.img_log_path = os.path.join(self.log_dir, "images")
+        self.tensorboard_log_path = os.path.join(self.log_dir, "tensorboard")
+        self.tensorboard_callback = callbacks.TensorBoard(self.tensorboard_log_path)
         os.makedirs(self.img_log_path, exist_ok=True)
-
-        self.gen_images = []
+        # Models object references
+        self.models = []
         self.dis_model = None
         self.gen_model = None
-        self.gan_mode = None
-
+        self.combined_mode = None
+        # Progress bar object reference
+        self.p_bar = None
+        # Data sampler variables
         self.data_index = 0
         self.indexes = np.arange(self.dataset_size, dtype="uint32")
         self.shuffle_indexes()
-
+        # rich.Console object
         self.console = console or Console()
 
+        self._batch_feed_dict = {}
+
+    def define_progress_bar(self):
+        """
+        Init rich.progress.Progress object to display different field information based on different GAN algorithms
+        """
+        raise NotImplementedError
 
     def load_generator(self, **kwargs):
         raise NotImplementedError
 
     def load_discriminator(self, **kwargs):
-        raise NotImplementedError        
+        raise NotImplementedError
+
+    def load_combined_model(self, **kwargs):
+        raise NotImplementedError
+
+    def process_dis_result(self, result):
+        """
+        Process results obtained from training discriminator and return as a dictionary
+        """
+        raise NotImplementedError
+
+    def process_gen_result(self, result):
+        """
+        Process results obtained from training combined_network and return as a dictionary
+        """
+        raise NotImplementedError
+
+    def generate_images(self, batch_size):
+        raise NotImplementedError
+
+    def generate_feed_dict(self, batch_size):
+        raise NotImplementedError
         
     def shuffle_indexes(self):
         np.random.shuffle(self.indexes)
@@ -46,83 +79,65 @@ class BaseGAN:
     def sample_latent(self, batch_size):
         latent = np.random.randn(batch_size, self.latent_size)
         return latent
-        
-    def sample_real_data(self, batch_size):
-        indexes = self.indexes[self.data_index:self.data_index+batch_size]
-        data = self.dataset[indexes]
+
+    def increment_index(self, batch_size):
         self.data_index += batch_size
         if self.data_index >= self.dataset_size:
             self.data_index = 0
-            self.shuffle_indexes()
-        return data
-    
-    def sample_fake_data(self, batch_size):
-        latent = self.sample_latent(batch_size)
-        data = self.gen_model.predict(latent)
-        return data
-    
-    def sample_data(self, batch_size):
-        fake_data = self.sample_fake_data(batch_size)
-        real_data = self.sample_real_data(batch_size)
-        data = np.concatenate((fake_data, real_data), axis=0)
-        labels = np.concatenate((np.zeros(fake_data.shape[0]), np.ones(real_data.shape[0])), axis=0)
-        return data, labels
 
-    def gather_logs(self, var_dict):
-        def remove_prefix(key):
-            return key[len(self.log_prefix):]
-        return {remove_prefix(k):v for k, v in var_dict.items() if k.startswith(self.log_prefix)}
+    def _train_discriminator(self):
+        result = self.dis_model.train_on_batch(self._batch_feed_dict["dis_inputs"], self._batch_feed_dict["dis_outputs"])
+        result_dict = self.process_dis_result(result)
+        return result_dict
 
-    def store_logs(self, log_dict):
-        for k, v in log_dict.items():
-            self.history[k].append(v)
+    def _train_generator(self):
+        result = self.combined_model.train_on_batch(self._batch_feed_dict["gen_inputs"], self._batch_feed_dict["gen_outputs"])
+        result_dict = self.process_gen_result(result)
+        return result_dict
 
     def train_once(self, batch_size):
-        # Train discriminator
-        batch_data, batch_labels = self.sample_data(batch_size)
-        log_dis_loss, log_dis_acc = self.dis_model.train_on_batch(batch_data, batch_labels)
-        # Train generator
-        latent_vectors = self.sample_latent(2*batch_size)
-        log_gen_loss = self.gan_model.train_on_batch(latent_vectors, np.ones((latent_vectors.shape[0], 1)))
-        log_dict = self.gather_logs(vars())
-        return log_dict
+        self.generate_feed_dict(batch_size)
+        dis_result = self._train_discriminator()
+        gen_result = self._train_generator()
+        self.increment_index(batch_size)
+        return {**dis_result, **gen_result}
 
     def train(self, epochs=100, batch_size=32, evaluate_interval=10):
-        # TODO: Is it really necessary to display all of these in the progress bar??
+        for model in self.models:
+            self.tensorboard_callback.set_model(model)
         train_per_epoch = np.math.ceil(self.dataset_size/batch_size)
-        # p_bar = ProgressBar(total_iter=epochs*train_per_epoch, display_text="Training:", display_interval=10)
-        step_text_fmt = (":: Epoch: [cyan]({task.fields[epoch]}/{task.fields[total_epoch]})[/cyan] :: dis (loss, acc): [cyan]({task.fields[dis_loss]:.4f}, {task.fields[dis_acc]:.4f})[/cyan] "
-                         ":: gen loss: [cyan]{task.fields[gen_loss]:.4f}[/cyan]")
-        p_bar = Progress(TextColumn("{task.description}"),
-                         BarColumn(complete_style="bold yellow", finished_style="bold cyan"),
-                         "[progress.percentage]{task.percentage:>3.1f}%",
-                         ":: Time left:",
-                         TimeRemainingColumn(),
-                         TextColumn(step_text_fmt),
-                         refresh_per_second=2)
+        self.define_progress_bar()
         print()
         self.console.rule(f"Training for [cyan]{epochs}[/cyan] epochs with [cyan]{train_per_epoch}[/cyan] iterations per epoch")
-        with p_bar:
-            task = p_bar.add_task(f" Training:", total=epochs*train_per_epoch, visible=False, epoch=0, total_epoch=epochs, dis_loss=0, dis_acc=0, gen_loss=0)
+        with self.p_bar:
+            task = self.p_bar.add_task(f" Training:", total=epochs*train_per_epoch, visible=False, total_epoch=epochs)
             for epoch in range(1, epochs+1):
+                self.shuffle_indexes()
                 for batch_iter in range(train_per_epoch):
                     log_dict = self.train_once(batch_size)
+                    # TODO: Implement code to log train results
+                    self.tensorboard_callback.on_train_batch_end(batch_iter, logs=log_dict)
                     self.store_logs(log_dict)
-                    p_bar.update(task, advance=1, visible=True, epoch=epoch, **log_dict)
-                    # p_bar.step(add_text=step_text_fmt.format(epoch=epoch, **log_dict))
+                    self.p_bar.update(task, advance=1, visible=True, epoch=epoch, **log_dict)
                 if (epoch == epochs) or (epoch%evaluate_interval == 0):
                     self.save_images(epoch)
-        meta_data = {"epochs": epochs, "latent_size": self.latent_size, "batch_size": batch_size, "evaluate_interval": evaluate_interval}
         print()
         self.console.rule("Saving logs and models")
+        meta_data = {"epochs": epochs, "latent_size": self.latent_size, "batch_size": batch_size, "evaluate_interval": evaluate_interval}
         self.save_meta_data(meta_data)
         self.save_models()
         self.save_log()
         self.console.rule("Training finished")
 
+    def store_logs(self, log_dict):
+        for k, v in log_dict.items():
+            self.history[k].append(v)
+
     def save_images(self, epoch, cols=5):
+        import matplotlib
+        matplotlib.use('Agg')
         import matplotlib.pyplot as plt
-        gen_imgs = self.sample_fake_data(self.num_evaluates)
+        gen_imgs = self.generate_images(self.num_evaluates)
         rows = np.math.ceil(self.num_evaluates/cols)
         fig = plt.figure(figsize=(20, 4*rows))       # Each row has the size of 4
         for i, img in enumerate(gen_imgs, start=1):
@@ -134,9 +149,8 @@ class BaseGAN:
     def save_models(self):
         dump_path = os.path.join(self.log_dir, "models")
         os.makedirs(dump_path, exist_ok=True)
-        self.gen_model.save(os.path.join(dump_path, "generator.h5"))
-        self.dis_model.save(os.path.join(dump_path, "discriminator.h5"))
-        self.gan_model.save(os.path.join(dump_path, "combined.h5"))
+        for model in self.models:
+            model.save(os.path.join(dump_path, f"{model.name}.h5"))
         self.console.print(f"# Models saved in [cyan]'{dump_path}'[/cyan] directory.")
 
     def save_log(self):
@@ -148,6 +162,7 @@ class BaseGAN:
 
     def save_meta_data(self, data_dict):
         import json
+        # TODO: Save network structures too i.e. input and output info?
         dump_path = os.path.join(self.log_dir, "meta_data.json")
         with open(dump_path, "w") as outfile:
             json.dump(data_dict, outfile, indent=True)
